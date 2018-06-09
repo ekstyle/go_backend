43239b769ee88b24c29d04a6aa4d9fcd396f5f24
pozdnjakov <pozdnjakov@ekb.kassy.ru>
2018-06-09 11:58:41 +0500

    Init

---
+++ lib/model.go
@@ -0,0 +1,5 @@
+package lib
+
+type Exception struct {
+	Message string `json:"message"`
+}
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"encoding/json"
+	"./lib"
+
+	"log"
+)
+
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(lib.Exception{Message:"Empty"})
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", RootHandler)
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+}
